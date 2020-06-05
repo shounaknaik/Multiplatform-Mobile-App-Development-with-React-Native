5295b34b31074554086e5717fcc734632ca525c0
Shounak Naik <f20170835@goa.bits-pilani.ac.in>
2020-06-05 21:21:36 +0530

    Assignment 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Home  from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
 import DishDetail from './DishDetailComponent';
 import {View,Platform} from 'react-native';
 import {createStackNavigator,createDrawerNavigator} from 'react-navigation';
@@ -37,6 +39,37 @@ const HomeNavigator=createStackNavigator({
     }
 });
 
+const ContactNavigator=createStackNavigator({
+    Contact:{screen:Contact},
+    
+},{
+    
+    navigationOptions:{
+        headerStyle:{
+            backgroundColor:'#512DA8'
+        },
+        headerTintColor:'#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+const AboutNavigator=createStackNavigator({
+    About:{screen:About},
+    
+},{
+    
+    navigationOptions:{
+        headerStyle:{
+            backgroundColor:'#512DA8'
+        },
+        headerTintColor:'#fff',
+        headerTitleStyle: {
+            color: '#fff'
+        }
+    }
+});
+
 const MainNavigator=createDrawerNavigator({
     Home:{
         screen:HomeNavigator,
@@ -50,6 +83,20 @@ const MainNavigator=createDrawerNavigator({
         navigationOptions:{
             title:'Menu',
             drawerLabel:'Menu'
+        }
+    },
+    Contact:{
+        screen:ContactNavigator,
+        navigationOptions:{
+            title:'Contact Us',
+            drawerLabel:'Contact Us'
+        }
+    },
+    About:{
+        screen:AboutNavigator,
+        navigationOptions:{
+            title:'About Us',
+            drawerLabel:'About Us'
         }
     }
 },{
