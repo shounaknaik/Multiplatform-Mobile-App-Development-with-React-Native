9b6ed5566e82c8d27b8d67365a3990e5f0d120db
Shounak Naik <f20170835@goa.bits-pilani.ac.in>
2020-06-10 18:36:52 +0530

    Picking an Image

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -160,6 +160,8 @@ const LoginNavigator=createStackNavigator({
         headerTitleStyle: {
             color: '#fff'
         },
+        title:'Login',
+        headerTintColor:'#fff',
         headerLeft: <Icon name="menu" size={24}
             color="white"
             onPress={()=>navigation.toggleDrawer()}/>
